5a2740a359fdb1470666c232cefc9560f0cb34d9
kaffu0424 <kaffu@naver.com>
2023-07-10 13:10:48 +0900

    acmicpc1932
    백준 1932번 문제
    실버1
    문제 분류 DP

---
+++ acmicpc/acmicpc_1932/acmicpc_1932/acmicpc_1932.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+//백준 1932번
+vector<vector<int>> v;
+vector<vector<int>> cache;
+int n;
+
+int cal(int y, int x)
+{
+	if (y >= n)
+		return 0;
+
+
+	int& ret = cache[y][x];
+	if (ret != 0)
+		return ret;
+
+
+	ret = cal(y + 1, x) > cal(y + 1, x + 1) ? cal(y + 1, x) : cal(y + 1, x + 1);
+	ret += v[y][x];
+	return ret;
+}
+
+int main()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		v.resize(n);
+		cache.resize(n);
+		for (int j = 0; j < i + 1; j++)
+		{
+			int num;
+			cin >> num;
+			v[i].push_back(num);
+			cache[i].push_back(0);
+		}
+	}
+
+	cout << cal(0, 0);
+}
