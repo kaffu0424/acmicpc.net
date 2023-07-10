944edb166e26089903a20535614fe17480085310
kaffu0424 <kaffu@naver.com>
2023-07-10 13:13:50 +0900

    acmicpc 9095
    백준 9095번

---
+++ acmicpc/acmicpc_9095/acmicpc_9095/acmicpc_9095.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+// 백준 9095
+int dp[101];
+int combination(int n, int target)
+{
+	if (n == target)
+		return 1;
+	if (n > target)
+		return 0;
+
+	if (dp[n] != -1)
+		return dp[n];
+
+	dp[n] = combination(n + 1, target) + combination(n + 2, target) + combination(n + 3, target);
+	return dp[n];
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+	while (T--)
+	{
+		for (int i = 0; i < 101; i++)
+		{
+			dp[i] = -1;
+		}
+		int n;
+		cin >> n;
+		cout << combination(0, n) << "\n";
+	}
+}
