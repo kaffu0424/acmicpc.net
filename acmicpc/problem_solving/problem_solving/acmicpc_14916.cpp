3feb8ebf98fb407fc2ad5f3e758331ea9c3da7b9
kaffu0424 <kaffu@naver.com>
2023-07-12 14:21:45 +0900

    14916
    https://www.acmicpc.net/problem/14916

---
+++ acmicpc/problem_solving/problem_solving/acmicpc_14916.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int>dp;
+int result;
+int N;
+int solving(int n)
+{
+	dp.push_back(-1);
+	dp.push_back(-1);
+	dp.push_back(1);
+	dp.push_back(-1);
+	dp.push_back(2);
+	dp.push_back(1);
+	dp.push_back(3);
+	dp.push_back(2);
+	dp.push_back(4);
+
+	for (int i = 9; i <= n; i++)
+	{
+		dp.push_back(dp[i - 5] + 1);
+	}
+	return dp[n];
+}
+
+int main()
+{
+	cin >> N;
+	result = solving(N);
+	cout << result;
+}
