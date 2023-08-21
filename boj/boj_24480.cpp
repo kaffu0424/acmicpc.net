11fdde9ef8019e859539d561fb4fe289f4fe1f57
kaffu0424 <kaffu@naver.com>
2023-08-21 16:52:44 +0900

    24479 24480
    백준 24479, 24480번
    깊이우선탐색 연습문제

---
+++ boj/boj_24480.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> graph[100001];
+int visit[100001];
+int N, M, R;
+int u, v;
+int cnt = 0;
+
+void dfs(int start)
+{
+	cnt++;
+	visit[start] = cnt;
+	for (int i : graph[start])
+	{
+		if (visit[i] == 0)
+			dfs(i);
+	}
+}
+
+bool comp(int a, int b)
+{
+	return a > b;
+}
+
+int main()
+{
+
+	cin >> N >> M >> R;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> v >> u;
+		graph[v].push_back(u);
+		graph[u].push_back(v);
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		sort(graph[i].begin(), graph[i].end(), comp);
+	}
+	dfs(R);
+
+	for (int i = 1; i <= N; i++)
+		cout << visit[i] << "\n";
+}
